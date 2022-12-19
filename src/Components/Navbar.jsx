59da250678d68e4d8b708f33178b2a36020333a1
jellywiz <muzaffer.dler@gmail.com>
2022-12-19 18:21:53 +0300

    Upadte navbar and styling

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,50 @@
+/* eslint-disable no-confusing-arrow */
+/* eslint-disable react/jsx-curly-newline */
+/* eslint-disable implicit-arrow-linebreak */
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from '../Img/planet.png';
+import './Navbar.css';
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="Navbar">
+      <div className="Logo-Container">
+        <img className="Logo" src={Logo} alt="" />
+        <h1>Space Travlers&#180; Hub</h1>
+      </div>
+      <div className="Nav-List">
+        <NavLink
+          className="Nav-Item"
+          to="/"
+          style={({ isActive }) =>
+            isActive ? { color: 'black' } : { color: 'blue' }
+          }
+        >
+          Rockets
+        </NavLink>
+        <NavLink
+          className="Nav-Item"
+          to="/missions"
+          style={({ isActive }) =>
+            isActive ? { color: 'black' } : { color: 'blue' }
+          }
+        >
+          Missions
+        </NavLink>
+        <h1>|</h1>
+        <NavLink
+          className="Nav-Item"
+          to="/profile"
+          style={({ isActive }) =>
+            isActive ? { color: 'black' } : { color: 'blue' }
+          }
+        >
+          My Profile
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
