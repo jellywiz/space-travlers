bc6981d79d214cac27551d72cdd7512464621121
jellywiz <muzaffer.dler@gmail.com>
2022-12-19 16:01:23 +0300

    Add redux structers

---
+++ src/Redux/configureStore.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+
+const bookReducers = combineReducers({
+  //
+});
+
+const store = configureStore({
+  // reducers
+});
+
+export default store;
