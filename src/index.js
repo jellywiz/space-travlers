c2c0561468363780c0ea2a300843b0fd439799b3
Emmanuel Mhango <emmanuel.mhango@bsu.edu.az>
2022-12-20 23:01:34 +0400

    Clear linter error on spacing for the index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<StrictMode>
+  <StrictMode>
     <Router>
       <Provider store={store}>
         <App />
