ae8b1e716c049105b776731059251d3d2b75d5c8
jellywiz <muzaffer.dler@gmail.com>
2022-12-21 13:22:35 +0300

    fix linter error

--- src/Components/MissionTable.test.js
+++ src/Components/MissionTable.test.js
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import MissionTable from './MissionTable';
-import store from '../redux/configureStore';
+import store from '../Redux/configureStore';
 
 it('Render table element on screen', () => {
   render(
