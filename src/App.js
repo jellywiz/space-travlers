c231497de5535d3c8e06e0561f97edb298164de9
jellywiz <muzaffer.dler@gmail.com>
2022-12-21 13:03:35 +0300

    Add dispatch for show mission in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
+import { showMissions } from './Redux/Missions/Missions';
 import NavBar from './Components/Navbar';
 import Rockets from './Redux/Rockets/RocketList';
 import Missions from './Pages/Missions';
 import Profile from './Pages/Profile';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(showMissions());
+  }, [dispatch]);
   return (
     <div className="App">
       <NavBar />
